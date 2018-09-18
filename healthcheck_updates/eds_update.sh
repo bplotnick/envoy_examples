f385e08868b7fd6959d46e9593a17a60e219ec98
Ben Plotnick <plotnick@yelp.com>
2018-09-18 14:53:39 -0700

    add scenario for adding an endpoint

--- healthcheck_updates/eds_update.sh
+++ healthcheck_updates/eds_update.sh
@@ -2,6 +2,11 @@
 # usage: ./eds_update.sh <eds_template_file> <hostname_of_service1>
 # example: ./eds_update.sh /etc/envoy/endpoints/service1_start.yaml.tmpl service1
 
-SERVICE1_IP=$(getent hosts $2 | tail -n1 | awk '{print $1}')
+SERVICE1_IP=$(getent hosts service1 | tail -n1 | awk '{print $1}')
+SERVICE2_IP=$(getent hosts service2 | tail -n1 | awk '{print $1}')
 
-sed "s/{{service1_ip}}/$SERVICE1_IP/g" $1 > /tmp/envoy/endpoints/service1.yaml
+cp $1 /tmp/endpoints.yaml
+sed -i "s/{{service1_ip}}/$SERVICE1_IP/g" /tmp/endpoints.yaml
+sed -i "s/{{service2_ip}}/$SERVICE2_IP/g" /tmp/endpoints.yaml
+
+mv /tmp/endpoints.yaml /tmp/envoy/endpoints/service1.yaml
